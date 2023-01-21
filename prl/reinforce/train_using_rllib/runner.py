8d5cc80ff64e4b2852d22113551afc3e99f56e32
sascha <hellovertex@outlook.com>
2023-01-21 18:19:37 +0100

    Added manually garbage collecting ray put objects at mem threshold

--- prl/reinforce/train_using_rllib/runner.py
+++ prl/reinforce/train_using_rllib/runner.py
@@ -35,9 +35,12 @@ class TrainRunner:
             # if checkpoint does not exist, we start from scratch
             print('No Rllib Algorithm Checkpoint exists at given directory, '
                   'we start training from scratch. Original Error Message was ', e)
-
+        import gc
+        import psutil
         # how many steps
         for i in range(100000000):
             results = algo.train()
+            if psutil.virtual_memory().percent >= 50:
+                gc.collect()
             if (i + 1) % ckpt_interval:
                 algo.save_checkpoint(algo_ckpt_dir)
