b3c3722c0f75b4b4441e74e8631d7455e451e174
sascha <hellovertex@outlook.com>
2023-03-17 18:27:13 +0100

    wip

--- prl/reinforce/tests/test_pettingzoo_env.py
+++ prl/reinforce/tests/test_pettingzoo_env.py
@@ -20,7 +20,9 @@ def get_cards(obs):
 def test_pettingzoo_env_final_step_reveals_cards_to_all_players():
     num_envs = 1
     agent_names = [RegisteredAgent.always_all_in.__name__,
-                   RegisteredAgent.always_fold.__name__]
+                   RegisteredAgent.always_fold.__name__,
+                   RegisteredAgent.always_fold.__name__ + '1',
+                   RegisteredAgent.always_fold.__name__ + '2', ]
     env_config = {}
     num_players = len(agent_names)
     env_config["env_wrapper_cls"] = AugmentObservationWrapper
@@ -38,27 +40,23 @@ def test_pettingzoo_env_final_step_reveals_cards_to_all_players():
         debug_reset_config_state_dict=None)
     all_in = 7
     fold = 0
-    action = np.zeros(1)
-    # get player 0 cards
-    observation = wrapped_env.reset()
-    obs = observation[0]['obs']
-    p0_cards = get_cards(obs)
-    # get player 1 cards
-    observation = wrapped_env.step(all_in)
-    obs = observation[0]['obs']
-    p1_cards = get_cards(obs)
-    assert p0_cards != p1_cards
-    # end game
-    observation = wrapped_env.step(fold)
-    obs = observation[0]['obs']
-    cards = get_cards(obs)
-    assert cards == p1_cards
+    obs0 = wrapped_env.reset()['obs']
+    obs1 = wrapped_env.step(all_in)[0]['obs']
+    obs2 = wrapped_env.step(fold)[0]['obs']
+    obs3 = wrapped_env.step(fold)[0]['obs']
+    obs4 = wrapped_env.step(fold)[0]['obs']
+    assert not np.array_equal(get_cards(obs0), get_cards(obs1))
+    assert not np.array_equal(get_cards(obs1), get_cards(obs2))
+    assert not np.array_equal(get_cards(obs2), get_cards(obs3))
+    # Game is over, observation 3,4 are both for last player
+    assert np.array_equal(get_cards(obs3), get_cards(obs4))
 
     # get player 0 cards
     obs_p0 = wrapped_env.env.observe(agent_names[0])['observation']
     cards = get_cards(obs_p0)
-    assert cards == p0_cards
+    assert np.array_equal(cards, p0_cards)
+
     # get player 1 cards
     obs_p1 = wrapped_env.env.observe(agent_names[1])['observation']
     cards = get_cards(obs_p1)
-    assert cards == p1_cards
+    assert np.array_equal(cards, p1_cards)
